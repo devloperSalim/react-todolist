b9df47ac4a5b6558d5d5ddccd403246b9c585b5b
salim_boulkhir <salimboulkhir123@gmail.com>
2024-04-27 13:11:39 +0100

    create first component function and class 'hello world'

---
+++ src/helloWorld/HelloWorld.js
@@ -0,0 +1,18 @@
+// function HelloWorld(){
+    
+//     return(
+//         <h1>hello world</h1>
+//     )
+// }
+// export default HelloWorld;
+
+
+//class component
+import { Component } from "react";
+
+export default class HelloWorld extends Component {
+
+    render(){
+        return <h1>hello world from class</h1>
+    }
+}
